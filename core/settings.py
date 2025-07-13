913106747d564004475e98d78853dc16ff0b4ac7
João <devnogg.yamamoto@gmail.com>
2025-07-13 13:10:33 -0300

    Criação do App/Domain

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',  # Custom app for user accounts
 ]
 
 MIDDLEWARE = [
